bc85577759c401e79f7f9079953bcfaa1e2559be
子杰 杨 <yangzijie@westlake.edu.cn>
2026-01-04 16:40:22 +0800

    fix: reorder syntax sidebar

--- docs/.vitepress/sidebars/en/syntax.mjs
+++ docs/.vitepress/sidebars/en/syntax.mjs
@@ -5,16 +5,16 @@ export default [
       { text: 'Overview', link: '/en/syntax/' },
       { text: 'Basic', link: '/en/syntax/basic' },
       { text: 'AIMD with Type', link: '/en/syntax/aimd-with-type' },
+      { text: 'Assigner', link: '/en/syntax/assigner' },
       { text: 'Protocol TOML', link: '/en/syntax/protocol_toml' },
+      { text: 'Variable Table', link: '/en/syntax/var_table' },
       { text: 'Multimedia', link: '/en/syntax/multimedia' },
       { text: 'Figures', link: '/en/syntax/fig' },
-      { text: 'Variable Table', link: '/en/syntax/var_table' },
       { text: 'References', link: '/en/syntax/ref' },
       { text: 'Citations', link: '/en/syntax/cite-in-aimd' },
-      { text: 'Assigner', link: '/en/syntax/assigner' },
-      { text: 'Assigner Type Constraints', link: '/en/syntax/assigner_type' },
       { text: 'Environment', link: '/en/syntax/env' },
       { text: 'Types', link: '/en/syntax/types' },
+      { text: 'Assigner Type Constraints', link: '/en/syntax/assigner_type' },
     ]
   }
 ]
