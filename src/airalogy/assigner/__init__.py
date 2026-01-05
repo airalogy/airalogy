23e5abaf3797d5e7a15c28db04e23bf2a0578f6e
Young Sherlock <xiayh17@gmail.com>
2026-01-05 23:06:48 +0800

    refactor: remove DependencyGraph module (will be separate PR)
    - Remove dependency_graph.py (not integrated with AssignerBase yet)
    - Clean up __init__.py exports
    - Keep PR focused on state mutation bug fix only

--- src/airalogy/assigner/__init__.py
+++ src/airalogy/assigner/__init__.py
@@ -1,19 +1,9 @@
 from .assigner_base import AssignerBase, DefaultAssigner, assigner
 from .assigner_result import AssignerResult
-from .dependency_graph import (
-    CyclicDependencyError,
-    DependencyGraph,
-    DependencyNode,
-    AssignerExecutor,
-)
 
 __all__ = [
     "AssignerBase",
     "DefaultAssigner",
     "AssignerResult",
     "assigner",
-    "CyclicDependencyError",
-    "DependencyGraph",
-    "DependencyNode",
-    "AssignerExecutor",
 ]
